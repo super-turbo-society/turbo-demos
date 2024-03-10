f1780147d5530f7b8a01f98f3588807cdc049539
jozanza <hello@jsavary.com>
2024-03-09 20:05:23 -0500

    pong update

--- pong/src/lib.rs
+++ pong/src/lib.rs
@@ -9,6 +9,8 @@ turbo::cfg! {r#"
 
 turbo::init! {
     struct GameState {
+        p1_score: u32,
+        p2_score: u32,
         paddle1: struct Paddle {
             x: f32,
             y: f32,
@@ -30,6 +32,8 @@ turbo::init! {
         let paddle_width = 8.0;
         let ball_radius = 4.0;
         Self {
+            p1_score: 0,
+            p2_score: 0,
             paddle1: Paddle { x: 10.0, y: h / 2.0 - paddle_height / 2.0, height: paddle_height },
             paddle2: Paddle { x: w - paddle_width - 10.0, y: h / 2.0 - paddle_height / 2.0, height: paddle_height },
             ball: Ball { x: w / 2.0, y: h / 2.0, velocity_x: 2.0, velocity_y: 2.0, radius: ball_radius },
@@ -48,6 +52,11 @@ turbo::go! {
 
     let gp1 = gamepad(0);
     let gp2 = gamepad(1);
+
+    // Debug log state
+    if gp1.start.pressed() || gp2.start.pressed() {
+        log!("{state:?}");
+    }
 
     // Move paddle 1
     if gp1.up.pressed() && state.paddle1.y > 0.0 {
@@ -69,6 +78,21 @@ turbo::go! {
     state.ball.x += state.ball.velocity_x;
     state.ball.y += state.ball.velocity_y;
 
+    // Ball out of bounds (scoring)
+    let did_p1_score = state.ball.x + state.ball.radius * 2.0 >= screen_w as f32;
+    if did_p1_score {
+        state.p1_score += 1;
+    }
+    let did_p2_score = state.ball.x < 0.0;
+    if did_p2_score {
+        state.p2_score += 1;
+    }
+    if did_p1_score || did_p2_score {
+        // Reset ball position
+        state.ball.x = screen_w as f32 / 2.0;
+        state.ball.y = screen_h / 2.0;
+    }
+
     // Ball collisions with paddles
     if (state.ball.x - state.ball.radius < state.paddle1.x + 8.0 &&
         state.ball.y > state.paddle1.y &&
@@ -82,13 +106,6 @@ turbo::go! {
     // Ball collisions with top and bottom
     if state.ball.y - state.ball.radius < 0.0 || state.ball.y + state.ball.radius > screen_h {
         state.ball.velocity_y = -state.ball.velocity_y;
-    }
-
-    // Ball out of bounds (scoring)
-    if state.ball.x < 0.0 || state.ball.x > screen_w as f32 {
-        // Reset ball position
-        state.ball.x = screen_w as f32 / 2.0;
-        state.ball.y = screen_h / 2.0;
     }
 
     // Draw paddles and ball
@@ -112,6 +129,8 @@ turbo::go! {
         d = state.ball.radius as u32,
         color = 0xffffffff
     );
+    text!(&format!("P1: {}", state.p1_score), font = Font::L, x = 64);
+    text!(&format!("P2: {}", state.p2_score), font = Font::L, x = (screen_w as i32 / 2) + 64);
 
     // Save game state for the next frame
     state.save();
