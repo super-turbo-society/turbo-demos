a73694fa3e961606a07fe489b060a847c59759bf
tsamisaherb <tsamisaherb@gmail.com>
2024-08-06 11:28:00 -0400

    Tweening Code

--- kiwis-fruit-tree/src/lib.rs
+++ kiwis-fruit-tree/src/lib.rs
@@ -23,6 +23,7 @@ turbo::init! {
         fruits: Vec<Fruit>,
         num_fruits_collected: usize,
     } = {
+        //TILES
         let mut tiles = Vec::new();
         for i in 0..(384 / TILE_SIZE) {
             for j in 0..3 {
@@ -34,6 +35,7 @@ turbo::init! {
         tiles.push(Tile::new(15,7));
         tiles.push(Tile::new(15,8));
         tiles.push(Tile::new(15,9));
+        //FRUITS
         let mut fruits = Vec::new();
         fruits.push(Fruit::new(10,5));
         fruits.push(Fruit::new(15,5));
@@ -207,24 +209,43 @@ struct Fruit{
     grid_y: usize,
     y_offset: f32,
     timer: f32,
-    is_collected: bool
+    is_collected: bool,
+    float_dist: f32,
+    float_tween: Tween<f32>,
 }
-
+//.duration(60).set(400.0).ease(Easing::EaseInBack)
 impl Fruit{
     fn new(grid_x: usize, grid_y: usize) -> Self {
-        Self { grid_x, grid_y, y_offset: 0., timer: 0., is_collected: false }
+        Self { grid_x, 
+            grid_y, 
+            y_offset: 0., 
+            timer: 0., 
+            is_collected: false,
+            float_dist: 2., 
+            float_tween:Tween::new(0.0).duration(60).ease(Easing::EaseInSine), 
+        }
     }
 
     fn update(&mut self) {
-        self.timer += 1.;
+        // self.timer += 1.;
 
-        let period = 90.0;
+        // let period = 90.0;
 
-        self.y_offset = 3.0 * (2.0 * std::f32::consts::PI * self.timer / period).sin();
+        // self.y_offset = 3.0 * (2.0 * std::f32::consts::PI * self.timer / period).sin();
 
-        if self.timer >= period {
-            self.timer -= period;
+        // if self.timer >= period {
+        //     self.timer -= period;
+        // }
+        if self.float_tween.done(){
+            if self.y_offset > 0.{
+                self.float_tween.set(-self.float_dist);
+            }
+            else{
+                self.float_tween.set(self.float_dist);
+            }
         }
+        self.y_offset = self.float_tween.get();
+        
     }
 
     fn contains(&self, point_x: f32, point_y: f32) -> bool {
@@ -326,8 +347,336 @@ turbo::go! {
 
     let text = format!("Fruits: {}", state.num_fruits_collected);
 
-    // Render the text at the top left corner (x: 10, y: 10) in white color
     text!(&text, x = 10+(cam!().0-192), y = 10+(cam!().1-108), font = Font::L, color = 0xffffffff);
     
     state.save();
+}
+
+
+
+//Tweening Code
+use std::collections::BTreeMap;
+
+use std::ops::Add;
+
+// Define easing function types
+#[derive(Debug, Default, Copy, Clone, Eq, PartialEq, BorshSerialize, BorshDeserialize)]
+enum Easing {
+    #[default]
+    Linear,
+    EaseInQuad,
+    EaseOutQuad,
+    EaseInOutQuad,
+    EaseInCubic,
+    EaseOutCubic,
+    EaseInOutCubic,
+    EaseInQuart,
+    EaseOutQuart,
+    EaseInOutQuart,
+    EaseInQuint,
+    EaseOutQuint,
+    EaseInOutQuint,
+    EaseInSine,
+    EaseOutSine,
+    EaseInOutSine,
+    EaseInExpo,
+    EaseOutExpo,
+    EaseInOutExpo,
+    EaseInCirc,
+    EaseOutCirc,
+    EaseInOutCirc,
+    EaseInBack,
+}
+
+#[allow(unused)]
+impl Easing {
+    pub const ALL: [Self; 23] = [
+        Self::Linear,
+        Self::EaseInQuad,
+        Self::EaseOutQuad,
+        Self::EaseInOutQuad,
+        Self::EaseInCubic,
+        Self::EaseOutCubic,
+        Self::EaseInOutCubic,
+        Self::EaseInQuart,
+        Self::EaseOutQuart,
+        Self::EaseInOutQuart,
+        Self::EaseInQuint,
+        Self::EaseOutQuint,
+        Self::EaseInOutQuint,
+        Self::EaseInSine,
+        Self::EaseOutSine,
+        Self::EaseInOutSine,
+        Self::EaseInExpo,
+        Self::EaseOutExpo,
+        Self::EaseInOutExpo,
+        Self::EaseInCirc,
+        Self::EaseOutCirc,
+        Self::EaseInOutCirc,
+        Self::EaseInBack,
+    ];
+    fn apply(&self, t: f64) -> f64 {
+        match *self {
+            Easing::Linear => t,
+            Easing::EaseInQuad => t * t,
+            Easing::EaseOutQuad => t * (2.0 - t),
+            Easing::EaseInOutQuad => {
+                if t < 0.5 {
+                    2.0 * t * t
+                } else {
+                    -1.0 + (4.0 - 2.0 * t) * t
+                }
+            }
+            Easing::EaseInCubic => t * t * t,
+            Easing::EaseOutCubic => {
+                let t = t - 1.0;
+                t * t * t + 1.0
+            }
+            Easing::EaseInOutCubic => {
+                if t < 0.5 {
+                    4.0 * t * t * t
+                } else {
+                    let t = t - 1.0;
+                    (t * t * t * 4.0) + 1.0
+                }
+            }
+            Easing::EaseInQuart => t * t * t * t,
+            Easing::EaseOutQuart => {
+                let t = t - 1.0;
+                1.0 - t * t * t * t
+            }
+            Easing::EaseInOutQuart => {
+                if t < 0.5 {
+                    8.0 * t * t * t * t
+                } else {
+                    let t = t - 1.0;
+                    1.0 - 8.0 * t * t * t * t
+                }
+            }
+            Easing::EaseInQuint => t * t * t * t * t,
+            Easing::EaseOutQuint => {
+                let t = t - 1.0;
+                t * t * t * t * t + 1.0
+            }
+            Easing::EaseInOutQuint => {
+                if t < 0.5 {
+                    16.0 * t * t * t * t * t
+                } else {
+                    let t = t - 1.0;
+                    1.0 + 16.0 * t * t * t * t * t
+                }
+            }
+            Easing::EaseInSine => 1.0 - (t * std::f64::consts::FRAC_PI_2).cos(),
+            Easing::EaseOutSine => (t * std::f64::consts::FRAC_PI_2).sin(),
+            Easing::EaseInOutSine => 0.5 * (1.0 - (std::f64::consts::PI * t).cos()),
+            Easing::EaseInExpo => {
+                if t == 0.0 {
+                    0.0
+                } else {
+                    (2.0 as f64).powf(10.0 * (t - 1.0))
+                }
+            }
+            Easing::EaseOutExpo => {
+                if t == 1.0 {
+                    1.0
+                } else {
+                    1.0 - (2.0 as f64).powf(-10.0 * t)
+                }
+            }
+            Easing::EaseInOutExpo => {
+                if t == 0.0 {
+                    0.0
+                } else if t == 1.0 {
+                    1.0
+                } else if t < 0.5 {
+                    (2.0 as f64).powf(10.0 * (2.0 * t - 1.0)) * 0.5
+                } else {
+                    (2.0 - (2.0 as f64).powf(-10.0 * (2.0 * t - 1.0))) * 0.5
+                }
+            }
+            Easing::EaseInCirc => 1.0 - (1.0 - t * t).sqrt(),
+            Easing::EaseOutCirc => (1.0 - (t - 1.0).powi(2)).sqrt(),
+            Easing::EaseInOutCirc => {
+                if t < 0.5 {
+                    0.5 * (1.0 - (1.0 - 4.0 * t * t).sqrt())
+                } else {
+                    0.5 * ((-((2.0 * t - 2.0).powi(2) - 1.0)).sqrt() + 1.0)
+                }
+            }
+            Easing::EaseInBack => {
+                let c1 = 1.70158;
+                let c3 = c1 + 1.;
+                c3 * t * t * t - c1 * t * t
+            }
+        }
+    }
+}
+
+// Define a generic Tween struct
+#[derive(Debug, Default, Copy, Clone, Eq, PartialEq, BorshSerialize, BorshDeserialize)]
+struct Tween<T> {
+    start: T,
+    end: T,
+    duration: usize,
+    elapsed: usize,
+    easing: Easing,
+    start_tick: Option<usize>,
+}
+
+#[allow(unused)]
+impl<T> Tween<T>
+where
+    T: Copy + Default + PartialEq + Interpolate<T> + Add<Output = T>,
+{
+    fn new(start: T) -> Self {
+        Self {
+            start,
+            end: start,
+            duration: 0,
+            elapsed: 0,
+            easing: Easing::default(),
+            start_tick: None,
+        }
+    }
+
+    fn duration(&mut self, duration: usize) -> Self {
+        self.duration = duration;
+        *self
+    }
+
+    fn ease(&mut self, easing: Easing) -> Self {
+        self.easing = easing;
+        *self
+    }
+
+    fn set_duration(&mut self, duration: usize) {
+        self.duration = duration;
+    }
+
+    fn set_ease(&mut self, easing: Easing) {
+        self.easing = easing;
+    }
+
+    fn set(&mut self, new_end: T) -> Self {
+        if new_end == self.end {
+            return *self;
+        }
+        self.start = self.get();
+        self.end = new_end;
+        self.elapsed = 0;
+        self.start_tick = Some(tick());
+        *self
+    }
+
+    fn add(&mut self, delta: T) {
+        self.start = self.get();
+        self.end = self.end + delta;
+        self.elapsed = 0;
+        self.start_tick = Some(tick());
+    }
+
+    fn get(&mut self) -> T {
+        if self.done() {
+            return self.end;
+        }
+        if self.start_tick.is_none() {
+            self.start_tick = Some(tick());
+        }
+        self.elapsed = tick() - self.start_tick.unwrap_or(0);
+        let t = self.elapsed as f64 / self.duration.max(1) as f64;
+        let eased_t = self.easing.apply(t);
+        T::interpolate(eased_t, self.start, self.end)
+    }
+
+    fn done(&self) -> bool {
+        self.duration == 0 || self.elapsed >= self.duration
+    }
+}
+
+trait Interpolate<T> {
+    fn interpolate(t: f64, start: T, end: T) -> T;
+}
+
+impl Interpolate<f32> for f32 {
+    fn interpolate(t: f64, start: f32, end: f32) -> f32 {
+        let n = start as f64 + (end as f64 - start as f64) * t;
+        n as f32
+    }
+}
+
+impl Interpolate<f64> for f64 {
+    fn interpolate(t: f64, start: f64, end: f64) -> f64 {
+        let n = start as f64 + (end as f64 - start as f64) * t;
+        n
+    }
+}
+
+impl Interpolate<usize> for usize {
+    fn interpolate(t: f64, start: usize, end: usize) -> usize {
+        let n = start as f64 + (end as f64 - start as f64) * t;
+        n as usize
+    }
+}
+
+impl Interpolate<isize> for isize {
+    fn interpolate(t: f64, start: isize, end: isize) -> isize {
+        let n = start as f64 + (end as f64 - start as f64) * t;
+        n as isize
+    }
+}
+
+impl Interpolate<u64> for u64 {
+    fn interpolate(t: f64, start: u64, end: u64) -> u64 {
+        let n = start as f64 + (end as f64 - start as f64) * t;
+        n as u64
+    }
+}
+
+impl Interpolate<i64> for i64 {
+    fn interpolate(t: f64, start: i64, end: i64) -> i64 {
+        let n = start as f64 + (end as f64 - start as f64) * t;
+        n as i64
+    }
+}
+
+impl Interpolate<u32> for u32 {
+    fn interpolate(t: f64, start: u32, end: u32) -> u32 {
+        let n = start as f64 + (end as f64 - start as f64) * t;
+        n as u32
+    }
+}
+
+impl Interpolate<i32> for i32 {
+    fn interpolate(t: f64, start: i32, end: i32) -> i32 {
+        let n = start as f64 + (end as f64 - start as f64) * t;
+        n as i32
+    }
+}
+
+impl Interpolate<u16> for u16 {
+    fn interpolate(t: f64, start: u16, end: u16) -> u16 {
+        let n = start as f64 + (end as f64 - start as f64) * t;
+        n as u16
+    }
+}
+
+impl Interpolate<i16> for i16 {
+    fn interpolate(t: f64, start: i16, end: i16) -> i16 {
+        let n = start as f64 + (end as f64 - start as f64) * t;
+        n as i16
+    }
+}
+
+impl Interpolate<u8> for u8 {
+    fn interpolate(t: f64, start: u8, end: u8) -> u8 {
+        let n = start as f64 + (end as f64 - start as f64) * t;
+        n as u8
+    }
+}
+
+impl Interpolate<i8> for i8 {
+    fn interpolate(t: f64, start: i8, end: i8) -> i8 {
+        let n = start as f64 + (end as f64 - start as f64) * t;
+        n as i8
+    }
 }
