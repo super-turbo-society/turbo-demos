9bab6776d5c285588d53cdc990066f403c1dd37d
jozanza <hello@jsavary.com>
2025-07-30 13:25:25 -0400

    update space invaders

--- space-invaders/src/lib.rs
+++ space-invaders/src/lib.rs
@@ -1,5 +1,17 @@
 use turbo::*;
 
+#[turbo::serialize]
+struct Invader {
+    x: f32,
+    y: f32,
+    moving_right: bool,
+    sprites: [String; 2],
+}
+#[turbo::serialize]
+struct Bullet {
+    x: f32,
+    y: f32,
+}
 
 #[turbo::game]
 struct GameState {
@@ -10,10 +22,8 @@ struct GameState {
     invader_direction_change: bool,
     score: u32,
     game_over: bool,
-    tick: usize,
     move_rate: usize,
 }
-
 impl GameState {
     fn new() -> Self {
         Self {
@@ -38,11 +48,11 @@ impl GameState {
             invader_direction_change: false,
             score: 0,
             game_over: false,
-            tick: 0,
             move_rate: 10,
         }
     }
     fn update(&mut self) {
+        let t = time::tick();
         let won_game = self.invaders.is_empty();
         let lost_game = self.game_over;
 
@@ -70,7 +80,7 @@ impl GameState {
 
             // Move invaders and check for direction change
             let mut hit_edge = false;
-            if self.tick % self.move_rate == 0 {
+            if t % self.move_rate == 0 {
                 for invader in &mut self.invaders {
                     let canvas_w = 224.0 as f32;
                     invader.x += if invader.moving_right { 2.0 } else { -2.0 };
@@ -85,7 +95,7 @@ impl GameState {
             }
 
             // Increase move rate every 10s
-            if self.tick % 600 == 0 {
+            if t % 600 == 0 {
                 let increase = 1; // 6 frames (game runs at 60 frames per second).
                 let minimum = 1; // This is the smallest frame delay between movement.
                 self.move_rate = self.move_rate.saturating_sub(increase).max(minimum);
@@ -127,7 +137,7 @@ impl GameState {
         // Draw the invaders
         for invader in &self.invaders {
             // Change sprite every .5s
-            let sprite_index = if self.tick % 60 < 30 { 0 } else { 1 };
+            let sprite_index = if t % 60 < 30 { 0 } else { 1 };
             let sprite_name = &invader.sprites[sprite_index];
             sprite!(sprite_name, x = invader.x, y = invader.y);
         }
@@ -141,7 +151,6 @@ impl GameState {
         text!("SCORE: {:0>5}", self.score; x = 10, y = 10, font = "large", color = 0xffffffff);
 
         if won_game {
-            // TODO: draw game over text
             text!(
                 "YOU WIN!",
                 x = 80,
@@ -151,7 +160,6 @@ impl GameState {
             );
         }
         if lost_game {
-            // TODO: draw game over text
             text!(
                 "GAME OVER",
                 x = 76,
@@ -160,21 +168,5 @@ impl GameState {
                 color = 0xffffffff
             );
         }
-
-        // Save game self for the next frame
-        self.tick += 1;
     }
-}
-
-#[turbo::serialize]
-struct Invader {
-    x: f32,
-    y: f32,
-    moving_right: bool,
-    sprites: [String; 2],
-}
-#[turbo::serialize]
-struct Bullet {
-    x: f32,
-    y: f32,
 }
