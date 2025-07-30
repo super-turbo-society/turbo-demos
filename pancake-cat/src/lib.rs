671276cb31fb217ab5b06ee617bc45fca3e64b8b
jozanza <hello@jsavary.com>
2025-07-30 12:59:12 -0400

    update pancake cat

--- pancake-cat/src/lib.rs
+++ pancake-cat/src/lib.rs
@@ -1,9 +1,16 @@
 use turbo::*;
 
+#[turbo::serialize]
+struct Pancake {
+    x: f32,
+    y: f32,
+    vel: f32,
+    radius: f32,
+}
+
 #[turbo::game]
 struct GameState {
-    frame: u32,
-    last_munch_at: u32,
+    last_munch_at: usize,
     cat_x: f32,
     cat_y: f32,
     cat_r: f32,
@@ -14,7 +21,6 @@ struct GameState {
 impl GameState {
     fn new() -> Self {
         Self {
-            frame: 0,
             last_munch_at: 0,
             cat_x: 128.0,
             cat_y: 112.0,
@@ -24,7 +30,6 @@ impl GameState {
         }
     }
     fn update(&mut self) {
-
         // Handle user input
         if gamepad::get(0).left.pressed() {
             self.cat_x -= 2.;
@@ -45,6 +50,9 @@ impl GameState {
             self.pancakes.push(pancake);
         }
 
+        // Get the current frame
+        let t = time::tick();
+
         // Update pancake positions and check for collisions with the cat
         let cat_center = (self.cat_x + self.cat_r, self.cat_y + self.cat_r);
         self.pancakes.retain_mut(|pancake| {
@@ -64,7 +72,7 @@ impl GameState {
             if radii_diff <= distance && distance <= radii_sum {
                 // Cat caught the pancake
                 self.score += 1;
-                self.last_munch_at = self.frame;
+                self.last_munch_at = t;
                 false // Remove the pancake from the game
             } else if pancake.y < 144. + (pancake.radius * 2.) {
                 true // Keep the pancake in the game if it's within the screen
@@ -77,19 +85,16 @@ impl GameState {
         clear(0x00ffffff);
 
         // Draw a tiled background of moving sprites
-        let frame = (self.frame as i32) / 2;
-        for col in 0..9 {
-            for row in 0..6 {
-                let x = col * 32;
-                let y = row * 32;
-                let x = ((x + frame) % (272 + 16)) - 32;
-                let y = ((y + frame) % (144 + 16)) - 24;
-                sprite!("heart", x = x, y = y);
-            }
-        }
+        sprite!(
+            "heart",
+            bounds = bounds::screen(),
+            repeat = true,
+            tx = t / 2,
+            ty = t / 2
+        );
 
         // Draw a speech bubble when the cat eats a pancake
-        if self.frame >= 64 && self.frame.saturating_sub(self.last_munch_at) <= 60 {
+        if t >= 64 && t.saturating_sub(self.last_munch_at) <= 60 {
             rect!(w = 30, h = 10, x = self.cat_x + 32.0, y = self.cat_y);
             circ!(d = 10, x = self.cat_x + 28.0, y = self.cat_y);
             rect!(w = 10, h = 5, x = self.cat_x + 28.0, y = self.cat_y + 5.0);
@@ -117,36 +122,25 @@ impl GameState {
                 y = pancake.y + 1.0,
                 d = pancake.radius + 2.,
                 color = 0x000000aa
-            ); // Render the pancakes
+            );
             circ!(
                 x = pancake.x,
                 y = pancake.y,
                 d = pancake.radius + 1.,
                 color = 0xf4d29cff
-            ); // Render the pancakes
+            );
             circ!(
                 x = pancake.x,
                 y = pancake.y,
                 d = pancake.radius,
                 color = 0xdba463ff
-            ); // Render the pancakes
+            );
         }
 
         // Draw the score
-        text!("Score: {}", self.score; x = 10, y = 10, font = "large", color = 0xffffffff); // Render the score
+        text!("Score: {}", self.score; x = 10, y = 10, font = "large", color = 0xffffffff);
 
         // Uncomment to print game self for debugging
         // text!(&format!("{:#?}", self), y = 24);
-
-        // Save game self for the next frame
-        self.frame += 1;
     }
-}
-
-#[turbo::serialize]
-struct Pancake {
-    x: f32,
-    y: f32,
-    vel: f32,
-    radius: f32,
 }
