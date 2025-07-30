fd6b3e0312a6a1060db82a72d5ddb19d56f1169b
jozanza <hello@jsavary.com>
2025-07-30 13:59:42 -0400

    update counter demo

--- counter-demo/src/lib.rs
+++ counter-demo/src/lib.rs
@@ -10,9 +10,7 @@ const BUTTON_TEXT_COLOR: u32 = 0xF0F8FFff;
 
 #[turbo::game]
 struct GameState;
-
 impl GameState {
-
     fn update(&mut self) {
         clear(BACKGROUND_COLOR);
 
@@ -23,8 +21,20 @@ impl GameState {
         draw_button(w, h, x_minus, y_minus);
         draw_button(w, h, x_plus, y_plus);
 
-        text!("-", x = 32, y = 187, font = "large", color = BUTTON_TEXT_COLOR);
-        text!("+", x = 94, y = 187, font = "large", color = BUTTON_TEXT_COLOR);
+        text!(
+            "-",
+            x = 32,
+            y = 187,
+            font = "large",
+            color = BUTTON_TEXT_COLOR
+        );
+        text!(
+            "+",
+            x = 94,
+            y = 187,
+            font = "large",
+            color = BUTTON_TEXT_COLOR
+        );
 
         let pointer = pointer::screen();
         if pointer.just_pressed() {
@@ -43,15 +53,37 @@ impl GameState {
                 id.to_string()
             };
             let user_line = format!("User: {}", truncated);
-            text!(&user_line, x = 10, y = 10, font = "medium", color = WHITE_COLOR);
+            text!(
+                &user_line,
+                x = 10,
+                y = 10,
+                font = "medium",
+                color = WHITE_COLOR
+            );
 
-            let user_path = format!("users/{}", id);
-            let user_count = counter::watch::<i32>(&user_path).unwrap_or(0);
+            let program_files_path =
+                std::path::PathBuf::new().join(counter::IncrementCounter::PROGRAM_ID);
+            let user_count_filepath = program_files_path.join("users").join(id);
+            let query = os::client::fs::watch(&user_count_filepath);
+            let user_count = query.parse().unwrap_or(0);
             let user_line = format!("Your Count: {}", user_count);
-            text!(&user_line, x = 10, y = 25, font = "medium", color = WHITE_COLOR);
+            text!(
+                &user_line,
+                x = 10,
+                y = 25,
+                font = "medium",
+                color = WHITE_COLOR
+            );
 
-            let global_count = counter::watch::<i32>("global_count").unwrap_or(0);
-            let color = if global_count < 0 { RED_COLOR } else { GREEN_COLOR };
+            let global_count_filepath = program_files_path.join("global_count");
+            let global_count = os::client::fs::watch(&global_count_filepath)
+                .parse()
+                .unwrap_or(0);
+            let color = if global_count < 0 {
+                RED_COLOR
+            } else {
+                GREEN_COLOR
+            };
             let global_line = format!("Global Count: {}", global_count);
             text!(&global_line, x = 10, y = 40, font = "medium", color = color);
         }
@@ -69,16 +101,14 @@ fn draw_button(w: i32, h: i32, x: i32, y: i32) {
     );
 }
 
-#[turbo::program]
 pub mod counter {
     use super::*;
 
-    #[turbo::command(name = "increment_counter")]
+    #[turbo::os::command(program = "counter", name = "increment_counter")]
     pub enum IncrementCounter {
         Plus(i32),
         Minus(i32),
     }
-
     impl IncrementCounter {
         pub fn amount(&self) -> i32 {
             match self {
@@ -87,24 +117,24 @@ pub mod counter {
             }
         }
     }
-
     impl CommandHandler for IncrementCounter {
         fn run(&mut self, user_id: &str) -> Result<(), std::io::Error> {
-            os::server::log!("Running increment command: {self:?}");
+            use os::server::*;
+            log!("Running increment command: {self:?}");
 
             let delta = self.amount();
 
             let user_path = format!("users/{}", user_id);
-            let mut user_counter = os::server::fs::read(&user_path).unwrap_or(0);
+            let mut user_counter = fs::read(&user_path).unwrap_or(0);
             user_counter += delta;
-            os::server::fs::write(&user_path, &user_counter)?;
+            fs::write(&user_path, &user_counter)?;
 
             let global_path = "global_count";
-            let mut global_counter = os::server::fs::read(global_path).unwrap_or(0);
+            let mut global_counter = fs::read(global_path).unwrap_or(0);
             global_counter += delta;
-            os::server::fs::write(global_path, &global_counter)?;
+            fs::write(global_path, &global_counter)?;
 
-            os::server::log!("Global Counter: {}", global_counter);
+            log!("Global Counter: {}", global_counter);
             Ok(())
         }
     }
